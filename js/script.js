6f3e03c3ca6f897c1a3d7aa251c0b728406f34f4
Gulchaman <gulchaman.keneshbekovna@gmail.com>
2021-11-21 14:22:02 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 "use strict";
 
-// const answer = prompt("Are you 18 years old?", "18");
-// console.log(answer + 5);
-// console.log(typeof(answer));
+const answer = prompt("Are you 18 years old?", "18");
+console.log(answer + 5);
+console.log(typeof(answer));
 
-// const result = [];
-// result[0] = prompt("Your name?", "");
-// result[1] = prompt("Your surname?", "");
-// result[2] = prompt("How old are you?", "");
+const result = [];
+result[0] = prompt("Your name?", "");
+result[1] = prompt("Your surname?", "");
+result[2] = prompt("How old are you?", "");
 
-// console.log(typeof(result));
+console.log(typeof(result));
 
-// const category = 'toys';
-// console.log(`https.//someurl.com/${category}/5`)
+const category = 'toys';
+console.log(`https.//someurl.com/${category}/5`)
 
  const isChecked = true, 
         isClosed =false;
