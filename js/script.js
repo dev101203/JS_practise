5659ce822a2e237c4e630ba35d2258ea8c0fbd48
Gulchaman <gulchaman.keneshbekovna@gmail.com>
2021-11-21 12:09:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// const answer = prompt("Are you 18 years old?", "18");
+// console.log(answer + 5);
+// console.log(typeof(answer));
+
+// const result = [];
+// result[0] = prompt("Your name?", "");
+// result[1] = prompt("Your surname?", "");
+// result[2] = prompt("How old are you?", "");
+
+// console.log(typeof(result));
+
+// const category = 'toys';
+// console.log(`https.//someurl.com/${category}/5`)
+
+ const isChecked = true, 
+        isClosed =false;
+
+console.log(isChecked && isClosed);
+
